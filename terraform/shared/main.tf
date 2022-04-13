397ffec00974a9c1eeb91917730ab1e228f2910f
Umair Fayaz <omayrfayaz@gmail.com>
2022-04-13 12:39:50 +0530

    BAH-1564 | terrraform fmt
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>

--- terraform/shared/main.tf
+++ terraform/shared/main.tf
@@ -10,8 +10,8 @@ terraform {
 
 # Grant bucket access: public
 resource "aws_s3_bucket_public_access_block" "publicaccess" {
-  bucket = aws_s3_bucket.demobucket.id
-  block_public_acls = false
+  bucket              = aws_s3_bucket.demobucket.id
+  block_public_acls   = false
   block_public_policy = false
 }
 
