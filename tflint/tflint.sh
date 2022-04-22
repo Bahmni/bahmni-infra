55ca36d3cc3677a6cad55cd6acd922453da3e208
MOHANKUMAR T <mohan13081999@gmail.com>
2022-04-22 18:07:33 +0530

    BAH-1614 | refactor. tflint script

--- tflint/tflint.sh
+++ tflint/tflint.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 
 set -e
-
+SCRIPT_PATH=$(realpath "${BASH_SOURCE[0]}")
+SCRIPT_DIRECTORY=$(dirname "$SCRIPT_PATH")
 GREEN_COLOR="\033[1;32m"
 YELLOW_COLOR="\033[1;33m"
 NO_COLOR="\033[0m"
 
-echo -e "${GREEN_COLOR}TFLint Scanning terraform/shared.... $NO_COLOR"
-cd terraform/shared
-if [[ $GITHUB_ACTIONS ]]
-then
-    terraform init -backend=false
-fi
-tflint --config=../../tflint/.tflint.hcl --loglevel=info --init
-tflint --config=../../tflint/.tflint.hcl --loglevel=info
-if [[ $? -eq 0 ]]
-then
-    echo -e "${YELLOW_COLOR}No issues found in terraform/shared.$NO_COLOR"
-fi
-cd ../../
-echo -e "\n"
+run_scan(){
+    folder=$1
 
-for folder in terraform/environment/*
-do
     cd $folder
     if [[ $GITHUB_ACTIONS ]]
     then
         terraform init -backend=false
     fi
+
     echo -e "${GREEN_COLOR}TFLint Scanning $folder....$NO_COLOR"
-    tflint --config=../../../tflint/.tflint.hcl --loglevel=info --init
-    tflint --config=../../../tflint/.tflint.hcl --loglevel=info
+    tflint --config=$SCRIPT_DIRECTORY/.tflint.hcl --loglevel=info --init
+    tflint --config=$SCRIPT_DIRECTORY/.tflint.hcl --loglevel=info
+
     if [[ $? -eq 0 ]]
     then
         echo -e "${YELLOW_COLOR}No issues found in $folder.$NO_COLOR"
     fi
-    cd ../../../
+
+    cd - > /dev/null
     echo -e "\n"
+}
+
+run_scan terraform/shared
+for folder in terraform/environment/*
+do
+    run_scan $folder
 done;
