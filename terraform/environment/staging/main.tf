88d091efb33ccadf0e4cef850f95852d62bd4b26
MOHANKUMAR T <mohan13081999@gmail.com>
2022-04-22 11:59:36 +0530

    BAH-1614 | add. required version attributes for terraform and aws provider

--- terraform/environment/staging/main.tf
+++ terraform/environment/staging/main.tf
@@ -3,6 +3,15 @@ provider "aws" {
 }
 
 terraform {
+  required_version = "~>1.1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.9.0"
+    }
+  }
+
   backend "s3" {
     key = "staging/terraform.tfstate"
   }
