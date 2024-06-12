3ca2bc26b5873076c957bdf38a68e4a9fa302460
MOHANKUMAR T <mohan13081999@gmail.com>
2024-06-12 13:55:25 +0530

    BAH-3902 | Add new custom parameter group for MySQL 8

--- terraform/modules/rds/rds_parameter_group.tf
+++ terraform/modules/rds/rds_parameter_group.tf
@@ -23,3 +23,29 @@ resource "aws_db_parameter_group" "custom_mysql_parameters" {
     value = var.mysql_time_zone
   }
 }
+
+resource "aws_db_parameter_group" "custom_mysql8_0_parameters" {
+  name   = "mysql8-0-custom-parameter-group-${var.environment}"
+  family = "mysql8.0"
+
+  parameter {
+    name         = "log_bin_trust_function_creators"
+    value        = 1
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "collation_server"
+    value = "utf8mb4_0900_ai_ci"
+  }
+
+  parameter {
+    name  = "time_zone"
+    value = var.mysql_time_zone
+  }
+}
